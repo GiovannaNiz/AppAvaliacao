54f050dcdf2ab360cd40fcd68b0e7ff4dafc8d09
GiovannaNiz <giovanna.santana20@etec.sp.gov.br>
2022-05-06 17:50:11 -0300

    dois

---
+++ app/src/main/java/com/example/appavaliacao/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.appavaliacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnTeste: Button = findViewById(R.id.btnTeste)
+        btnTeste.setOnClickListener { teste() }
+    }
+    private fun teste(){
+        val edt01: EditText = findViewById(R.id.edt01)
+        val edt02: EditText = findViewById(R.id.edt02)
+
+        if (edt01.text.isNotEmpty() && edt02.text.isNotEmpty()){
+            if (Integer.parseInt(edt01.text.toString()) > Integer.parseInt(edt02.text.toString())){
+                Toast.makeText(this, "Campo 1 é maior que o Campo 2", Toast.LENGTH_SHORT).show()
+            }else if (Integer.parseInt(edt01.text.toString()) == Integer.parseInt(edt02.text.toString())){
+                Toast.makeText(this, "Campo 1 é igual ao Campo 2", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Campo 2 é maior que o Campo 1", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
